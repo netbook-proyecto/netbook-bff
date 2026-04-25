c5636b9ecee59f13c14bec1ecc9715210fcdf556
Cristopher <cr.candiam@duouc.cl>
2026-04-25 19:39:34 -0400

    feat: configuracion inicial y archivo .gitignore

---
+++ bff/src/main/java/com/netbook/bff/modells/entities/Usuario.java
@@ -0,0 +1,29 @@
+package com.netbook.bff.modells.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+
+    @Column(nullable = false)
+    private String rut;
+
+    @Column(nullable = false, unique = true)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String correoInstitucional;
+
+}
